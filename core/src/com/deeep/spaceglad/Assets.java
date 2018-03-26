81c266d3925372f2d9dbf098567db6592a1d9007
NickAvery <35745096+NickAvery@users.noreply.github.com>
2018-03-26 15:59:28 -0700

    Add files via upload

---
+++ core/src/com/deeep/spaceglad/Assets.java
@@ -0,0 +1,47 @@
+package com.deeep.spaceglad;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.utils.UBJsonReader;
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
+
+/**
+ * Created by scanevaro on 01/08/2015.
+ */
+public class Assets {
+    public static Skin skin;
+	public static Model tvModel;
+	public static Model chairModel;
+	public static Model enemyModel;
+	public static Model playerModel;
+	
+	
+    public Assets() {
+        skin = new Skin();
+        FileHandle fileHandle = Gdx.files.internal("data/uiskin.json");
+        FileHandle atlasFile = fileHandle.sibling("uiskin.atlas");
+        if (atlasFile.exists()) {
+            skin.addRegions(new TextureAtlas(atlasFile));
+        }
+        skin.load(fileHandle);
+		
+		FileHandle tvPath = Gdx.files.internal("data/tv.g3db");
+		FileHandle chairPath = Gdx.files.internal("data/chair.g3db");
+		FileHandle enemyPath = Gdx.files.internal("data/enemy.g3db");
+		FileHandle playerPath = Gdx.files.internal("data/player.g3db");
+		UBJsonReader jsonReader = new UBJsonReader();
+		G3dModelLoader modelLoader = new G3dModelLoader(jsonReader);
+		
+		tvModel = modelLoader.loadModel(tvPath);
+		chairModel = modelLoader.loadModel(chairPath);
+		enemyModel = modelLoader.loadModel(enemyPath);
+		playerModel = modelLoader.loadModel(playerPath);
+    }
+
+    public static void dispose() {
+        skin.dispose();
+    }
+}
---
+++ core/src/com/deeep/spaceglad/managers/EntityFactory.java
@@ -0,0 +1,134 @@
+package com.deeep.spaceglad.managers;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.BoundingBox;
+import com.badlogic.gdx.physics.bullet.collision.*;
+import com.badlogic.gdx.physics.bullet.dynamics.btKinematicCharacterController;
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
+import com.deeep.spaceglad.bullet.MotionState;
+import com.deeep.spaceglad.components.*;
+import com.deeep.spaceglad.systems.BulletSystem;
+import com.deeep.spaceglad.GameWorld;
+import com.deeep.spaceglad.Assets;
+/**
+ * Created by Elmar on 7-8-2015.
+ */
+public class EntityFactory {
+    private static Model playerModel;
+    private static Texture playerTexture;
+    private static ModelBuilder modelBuilder;
+    private static Model boxModel;
+
+    static {
+        modelBuilder = new ModelBuilder();
+        playerTexture = new Texture(Gdx.files.internal("data/badlogic.jpg"));
+        Material material = new Material(TextureAttribute.createDiffuse(playerTexture), ColorAttribute.createSpecular(1, 1, 1, 1), FloatAttribute.createShininess(8f));
+        playerModel = modelBuilder.createCapsule(2f, 6f, 16, material, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
+        boxModel = modelBuilder.createBox(1f, 1f, 1f, new Material(ColorAttribute.createDiffuse(Color.WHITE),
+                ColorAttribute.createSpecular(Color.WHITE), FloatAttribute.createShininess(64f)), VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+    }
+
+
+    public static Entity createStaticEntity(Model model, float x, float y, float z, float rotateDeg) {
+        final BoundingBox boundingBox = new BoundingBox();
+        model.calculateBoundingBox(boundingBox);
+        Vector3 tmpV = new Vector3();
+        btCollisionShape col = new btBoxShape(tmpV.set(boundingBox.getWidth() * 0.5f, boundingBox.getHeight() * 0.5f, boundingBox.getDepth() * 0.5f));
+        Entity entity = new Entity();
+        ModelComponent modelComponent = new ModelComponent(model, x, y, z);
+		modelComponent.instance.transform.rotate(0f, 0f, 1f, rotateDeg);
+		modelComponent.instance.calculateTransforms();
+        entity.add(modelComponent);
+        BulletComponent bulletComponent = new BulletComponent();
+        bulletComponent.bodyInfo = new btRigidBody.btRigidBodyConstructionInfo(0, null, col, Vector3.Zero);
+        bulletComponent.body = new btRigidBody(bulletComponent.bodyInfo);
+        bulletComponent.body.userData = entity;
+        bulletComponent.motionState = new MotionState(modelComponent.instance.transform);
+        ((btRigidBody) bulletComponent.body).setMotionState(bulletComponent.motionState);
+        entity.add(bulletComponent);
+        return entity;
+    }
+	
+	public static Entity createDoorEntity(Model model, float x, float y, float z, float rotateDeg, Door d) {
+		final BoundingBox boundingBox = new BoundingBox();
+        model.calculateBoundingBox(boundingBox);
+        Vector3 tmpV = new Vector3();
+        btCollisionShape col = new btBoxShape(tmpV.set(boundingBox.getWidth() * 0.5f, boundingBox.getHeight() * 0.5f, boundingBox.getDepth() * 0.5f));
+        Entity entity = new Entity();
+        ModelComponent modelComponent = new ModelComponent(model, x, y, z);
+		modelComponent.instance.transform.rotate(0f, 1f, 0f, rotateDeg);
+		modelComponent.instance.calculateTransforms();
+        entity.add(modelComponent);
+        BulletComponent bulletComponent = new BulletComponent();
+        bulletComponent.bodyInfo = new btRigidBody.btRigidBodyConstructionInfo(0, null, col, Vector3.Zero);
+        bulletComponent.body = new btRigidBody(bulletComponent.bodyInfo);
+        bulletComponent.body.userData = entity;
+        bulletComponent.motionState = new MotionState(modelComponent.instance.transform);
+        ((btRigidBody) bulletComponent.body).setMotionState(bulletComponent.motionState);
+        entity.add(bulletComponent);
+        entity.add(new DoorComponent(d));
+        return entity;
+	}
+
+    private static Entity createCharacter(BulletSystem bulletSystem, float x, float y, float z, int type) {
+        Entity entity = new Entity();
+		ModelComponent modelComponent = null;
+		switch(type) {
+			case 0: //player
+				modelComponent = new ModelComponent(Assets.playerModel, x, y, z);	
+				modelComponent.instance.transform.rotate(0, 1, 0, 180);		
+				modelComponent.instance.calculateTransforms();				
+			break;
+			case 1: //enemy
+				modelComponent = new ModelComponent(Assets.enemyModel, x, y, z);
+			break;
+		}
+		if (modelComponent != null) {
+			entity.add(modelComponent);
+		}
+        CharacterComponent characterComponent = new CharacterComponent();
+        characterComponent.ghostObject = new btPairCachingGhostObject();
+        characterComponent.ghostObject.setWorldTransform(modelComponent.instance.transform);
+        characterComponent.ghostShape = new btCapsuleShape(2f, 2f);
+        characterComponent.ghostObject.setCollisionShape(characterComponent.ghostShape);
+        characterComponent.ghostObject.setCollisionFlags(btCollisionObject.CollisionFlags.CF_CHARACTER_OBJECT);
+        characterComponent.characterController = new btKinematicCharacterController(characterComponent.ghostObject, characterComponent.ghostShape, .35f);
+        characterComponent.ghostObject.userData = entity;
+        entity.add(characterComponent);
+        bulletSystem.collisionWorld.addCollisionObject(entity.getComponent(CharacterComponent.class).ghostObject,
+                (short) btBroadphaseProxy.CollisionFilterGroups.CharacterFilter,
+                (short) (btBroadphaseProxy.CollisionFilterGroups.AllFilter));
+        bulletSystem.collisionWorld.addAction(entity.getComponent(CharacterComponent.class).characterController);
+        return entity;
+    }
+
+    public static Entity createPlayer(BulletSystem bulletSystem, float x, float y, float z) {
+        Entity entity = createCharacter(bulletSystem, x, y, z, 0);
+        entity.add(new PlayerComponent());
+        return entity;
+    }
+
+    public static Entity createEnemy(BulletSystem bulletSystem, float x, float y, float z) {
+        Entity entity = createCharacter(bulletSystem, x, y, z, 1);
+        entity.add(new EnemyComponent(EnemyComponent.STATE.HUNTING));
+        entity.add(new StatusComponent());
+        return entity;
+    }
+
+//    public static void dispose() {
+//        playerModel.dispose();
+//        playerTexture.dispose();
+//        boxModel.dispose();
+//    }
+}
