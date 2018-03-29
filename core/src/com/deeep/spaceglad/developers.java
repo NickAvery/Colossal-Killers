264a4fe872bd65b185760cde2c34c01f45d301c1
Kevin Dorscher <kevindorscher@yahoo.com>
2018-03-29 12:15:03 -0700

    Auto stash before rebase of "origin/master"

--- core/src/com/deeep/spaceglad/developers.java
+++ core/src/com/deeep/spaceglad/developers.java
@@ -3,6 +3,7 @@ public class dev_emails
   private String [] email = new String [9];
     email[0] = "tayl2957@vandals.uidaho.edu";
     email[1] = "sonn6508@vandals.uidaho.edu";
+	email[2] = "dors7905@vandals.uidaho.edu";
 }
 
 public class dev_names
@@ -10,4 +11,5 @@ public class dev_names
  private String [] names = new String [9];  
   names[0] = "Jackson Taylor";
   names[1] = "Tim Sonnen";
+  names[2] = "Kevin Dorscher";
 }
