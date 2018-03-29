e33f206f4f24321e973ce7c79246bd0acf19dc9d
Tim Sonnen <tsonnen@users.noreply.github.com>
2018-03-29 12:11:40 -0700

    Add name to developers.java

--- core/src/com/deeep/spaceglad/developers.java
+++ core/src/com/deeep/spaceglad/developers.java
@@ -2,10 +2,12 @@ public class dev_emails
 {
   private String [] email = new String [9];
     email[0] = "tayl2957@vandals.uidaho.edu";
+    email[1] = "sonn6508@vandals.uidaho.edu";
 }
 
 public class dev_names
 {
  private String [] names = new String [9];  
   names[0] = "Jackson Taylor";
+  names[1] = "Tim Sonnen";
 }
