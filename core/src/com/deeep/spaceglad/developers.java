018a90bf4116defedaa6c9973c33f6288db9763b
jacklazarus <tayl2957@vandals.uidaho.edu>
2018-03-29 14:10:14 -0500

    Update developers.java

--- core/src/com/deeep/spaceglad/developers.java
+++ core/src/com/deeep/spaceglad/developers.java
@@ -1,1 +1,11 @@
+public class dev_emails
+{
+  private String [] email = new String [9];
+    email[0] = "tayl2957@vandals.uidaho.edu";
+}
 
+public class dev_names
+{
+ private String [] names = new String [9];  
+  names[0] = "Jackson Taylor";
+}
