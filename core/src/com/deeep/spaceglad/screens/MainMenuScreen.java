67e1c1c4ae358203aa02f222b9dc76598ba0efd5
DreamKontrol <barnetthu@gmail.com>
2018-04-12 19:17:14 +0000

    Fixed login screen

--- core/src/com/deeep/spaceglad/screens/MainMenuScreen.java
+++ core/src/com/deeep/spaceglad/screens/MainMenuScreen.java
@@ -68,7 +68,7 @@ public class MainMenuScreen implements Screen {
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game));
+                game.setScreen(new LoginScreen(game));
             }
         });
         leaderboardsButton.addListener(new ClickListener() {
