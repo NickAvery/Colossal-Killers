70d78a5ea1ccfd33da010ed12f60e1c17bf5a852
Nick Avery <35745096+NickAvery@users.noreply.github.com>
2018-04-11 18:22:46 -0700

    Needed to import all components
    Makes it easier to work with the entity system.

--- core/src/com/deeep/spaceglad/systems/EnemySystem.java
+++ core/src/com/deeep/spaceglad/systems/EnemySystem.java
@@ -7,9 +7,7 @@ import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 import com.deeep.spaceglad.GameWorld;
 import com.deeep.spaceglad.components.CharacterComponent;
-import com.deeep.spaceglad.components.EnemyComponent;
-import com.deeep.spaceglad.components.ModelComponent;
-import com.deeep.spaceglad.components.PlayerComponent;
+import com.deeep.spaceglad.components.*;
 import com.deeep.spaceglad.managers.EntityFactory;
 
 import java.util.Random;
