1214c324084a0a8110a3eec5ce60a1eb921dfbaa
Nick Avery <35745096+NickAvery@users.noreply.github.com>
2018-04-11 18:18:50 -0700

    Some changes to the monster system
    Monsters now spawn in one location where they won't fall off the map.
    If the monster's health is less than or equal to 0 it dies.

--- core/src/com/deeep/spaceglad/systems/EnemySystem.java
+++ core/src/com/deeep/spaceglad/systems/EnemySystem.java
@@ -40,8 +40,7 @@ public class EnemySystem extends EntitySystem implements EntityListener {
     public void update(float delta) {
         if (entities.size() < 1) {
             Random random = new Random();
-            engine.addEntity(EntityFactory.createEnemy(gameWorld.bulletSystem, random.nextInt(40) - 20, 10, random.nextInt(40) - 20));
-        }
+            engine.addEntity(EntityFactory.createEnemy(gameWorld.bulletSystem, 10, 3, 10,random.nextInt(2)+1));        }
         for (Entity e : entities) {
             ModelComponent mod = e.getComponent(ModelComponent.class);
             ModelComponent playerModel = player.getComponent(ModelComponent.class);
@@ -72,6 +71,10 @@ public class EnemySystem extends EntitySystem implements EntityListener {
             ghost.getTranslation(translation);
 
             mod.instance.transform.set(translation.x, translation.y, translation.z, rot.x, rot.y, rot.z, rot.w);
+            if (e.getComponent(EnemyComponent.class).health <= 0)
+			{
+				e.getComponent(StatusComponent.class).alive = false;
+			}
         }
     }
 
