6786568665dd8322e3cbcb647e06a4ae345c4b13
JamesMYoung <youn5393@vandals.uidaho.edu>
2018-04-16 09:52:33 +0900

    Correct files

--- core/src/com/deeep/spaceglad/managers/EntityFactory.java
+++ core/src/com/deeep/spaceglad/managers/EntityFactory.java
@@ -128,6 +128,36 @@ public class EntityFactory {
         entity.add(new PlayerComponent());
         return entity;
     }
+	
+	// Handles creating health packs
+	public static Entity createHealthPack(BulletSystem bulletSystem, float x, float y, float z) {
+        Entity entity = new Entity();
+		ModelComponent modelComponent = new ModelComponent(Assets.healthPackModel, x, y, z);
+		
+		final BoundingBox boundingBox = new BoundingBox();
+        Assets.healthPackModel.calculateBoundingBox(boundingBox);
+        Vector3 tmpV = new Vector3();
+        btCollisionShape col = new btBoxShape(tmpV.set(boundingBox.getWidth() * 0.5f, boundingBox.getHeight() * 0.5f, boundingBox.getDepth() * 0.5f));
+		
+		
+		BulletComponent bulletComponent = new BulletComponent();
+        bulletComponent.bodyInfo = new btRigidBody.btRigidBodyConstructionInfo(0, null, col, Vector3.Zero);
+        bulletComponent.body = new btRigidBody(bulletComponent.bodyInfo);
+        //bulletComponent.body.userData = entity;
+        //bulletComponent.motionState = new MotionState(modelComponent.instance.transform);
+        //((btRigidBody) bulletComponent.body).setMotionState(bulletComponent.motionState);
+        entity.add(bulletComponent);
+		
+		
+		HealthPackComponent healthPackComponent = new HealthPackComponent(HealthPackComponent.STATE.READY, 1);
+		entity.add(modelComponent);
+		entity.add(healthPackComponent);
+		
+		
+        
+        return entity;
+    }
+	
 
     public static Entity createEnemy(BulletSystem bulletSystem, float x, float y, float z, int type) {
         if (type != 1 && type != 2)
