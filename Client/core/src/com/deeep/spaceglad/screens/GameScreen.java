ca5cefc5f539407dd1120b8f1732fdf2d6b351ce
DreamKontrol <barnetthu@gmail.com>
2018-04-24 19:11:58 +0000

    Fixed message handling

--- Client/core/src/com/deeep/spaceglad/screens/GameScreen.java
+++ Client/core/src/com/deeep/spaceglad/screens/GameScreen.java
@@ -32,16 +32,44 @@ public class GameScreen implements Screen {
 	public void render(float delta) {
 
 		// Handle server messages
-		if (game.client != null) {
-			// Should only be null if offline
+		if (game.client != null) { // Should only be null if offline
 			while (game.client.getQueueLength() > 0) {
 				String msg = game.client.getNextMessage();
-				if (msg.startsWith("\\say ")) {
+				String params[] = msg.split(" ");
+				switch (params[0]) {
+				case "\\say":
 					gameUI.messageWidget.addChatMessage(msg.substring(5));
-				} else if (msg.startsWith("\\inform ")) {
+					break;
+				case "\\inform":
 					gameUI.messageWidget.addChatMessage("Server: " + msg.substring(8));
-				} else {
-				 // TODO Actually handle commands, don't just push them to the chat window
+					break;
+				case "\\avatar":
+					/*
+					 * params[0] = "\\avatar"
+					 * params[1] = "" <-- bug on server side
+					 * params[2] = username
+					 * params[3] = position.x
+					 * params[4] = position.y
+					 * params[5] = position.z
+					 * params[6] = min( rotation.y, 1.5 )
+					 * params[7] = "user\n"
+					 */
+					if (params[7] == "user\n") {
+						if (params[2] == game.client.username) {
+							// This player
+							// TODO
+						} else {
+							// Other Player
+							// TODO
+						}
+					} else {
+						// NPC
+						// TODO
+					}
+					break;
+				default:
+					// TODO Handle more commands
+					// Unhandled commands are pushed to chat window
 					gameUI.messageWidget.addChatMessage(msg);
 				}
 			}
